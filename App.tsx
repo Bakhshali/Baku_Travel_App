7268b413ac0372a4f1a66aa1117eb71b5e206c8c
alijanmirzayev <mirzeyevalican1@gmail.com>
2023-05-14 21:51:59 +0400

    fix home.tsx axios problem and add search button

--- App.tsx
+++ App.tsx
@@ -4,13 +4,16 @@ import { FirstLoginProvider } from './src/context/FirstLogin'
 import OpenScreen from './src/helpers/OpenScreen'
 import Main from './src/navigations/tab/Main'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { UserLocationProvider } from './src/context/UserLocation'
 
 export default function App() {
+  AsyncStorage.clear()
   return (
     <NavigationContainer>
       <FirstLoginProvider>
-        <OpenScreen />
-        {/* <Main/> */}
+        <UserLocationProvider>
+          <OpenScreen />
+        </UserLocationProvider>
       </FirstLoginProvider>
     </NavigationContainer>
   )
